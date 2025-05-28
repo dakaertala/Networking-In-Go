385056644ac1a7117d60b44aa6666545809fe663
Kaer Tala <khair_talap@outlook.com>
2025-05-28 08:37:44 +0500

    Added a method ReadFrom to TLV encoding struct

--- TlvEncoding.go
+++ TlvEncoding.go
@@ -26,7 +26,8 @@ type Payload interface {
 
 type Binary []byte
 
-func (m Binary) Bytes() []byte  { return m }
+func (m Binary) Bytes() []byte { return m }
+
 func (m Binary) String() string { return string(m) }
 
 func (m Binary) WriteTo(w io.Writer) (int64, error) {
@@ -43,6 +44,35 @@ func (m Binary) WriteTo(w io.Writer) (int64, error) {
 	n += 4
 
 	output, err := w.Write(m)
+
+	return n + int64(output), err
+}
+
+func (m *Binary) ReadFrom(r io.Reader) (int64, error) {
+	var typ uint8
+	err := binary.Read(r, binary.BigEndian, &typ)
+	if err != nil {
+		return 0, err
+	}
+
+	var n int64 = 1
+	if typ != BinaryType {
+		return n, errors.New("invalid Binary")
+	}
+
+	var size uint32
+	err = binary.Read(r, binary.BigEndian, &size)
+	if err != nil {
+		return n, err
+	}
+	n += 4
+
+	if size > MaxPayloadSize {
+		return n, ErrMaxPayloadSize
+	}
+
+	*m = make([]byte, size)
+	output, err := r.Read(*m)
 
 	return n + int64(output), err
 }
