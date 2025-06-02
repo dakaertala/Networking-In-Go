e126b36fcb46a4632bea42eb1ac25e5c29664864
Kaer Tala <khair_talap@outlook.com>
2025-06-02 18:45:34 +0500

    Server side of the TLV test

--- TLVEncodingString.go
+++ TLVEncodingString.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"io"
+	"net"
+	"testing"
 )
 
 type String string
@@ -62,4 +65,59 @@ func (m *String) ReadFrom(r io.Reader) (int64, error) {
 	*m = String(buf)
 
 	return n + int64(output), nil
+}
+
+func decode(r io.Reader) (Payload, error) {
+	var typ uint8
+	err := binary.Read(r, binary.BigEndian, &typ)
+	if err != nil {
+		return nil, err
+	}
+
+	var payload Payload
+
+	switch typ {
+	case BinaryType:
+		payload = new(Binary)
+	case StringType:
+		payload = new(String)
+	default:
+		return nil, errors.New("unkown type")
+	}
+
+	_, err = payload.ReadFrom(io.MultiReader(bytes.NewReader([]byte{typ}), r))
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
+
+func TestPayloads(t *testing.T) {
+	b1 := Binary("Clear is better than clever.")
+	b2 := binary("Don't panic.")
+	s1 := String("Errors are values.")
+	payloads := []Payload{&b1, &s1, &b2}
+
+	listener, err := net.Listen("tcp", "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer conn.Close()
+
+		for _, p := range payloads {
+			_, err := p.WriteTo(conn)
+			if err != nil {
+				t.Error(err)
+				break
+			}
+		}
+	}()
 }
