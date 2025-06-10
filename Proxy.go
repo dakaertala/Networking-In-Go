9ad53d1f5be25d69ffe25033859f0c8cd3370177
Kaer Tala <khair_talap@outlook.com>
2025-06-10 11:56:32 +0500

    Finishing up the test function of the proxy

--- Proxy.go
+++ Proxy.go
@@ -103,15 +103,9 @@ func TestProxy(t *testing.T) {
 
 					switch msg := string(buf[:n]); msg {
 					case "ping":
-						_, err := c.Write([]byte("pong"))
-						if err != nil {
-							t.Error(err)
-						}
+						_, err = c.Write([]byte("pong"))
 					default:
-						_, err := c.Write(buf[:n])
-						if err != nil {
-							t.Error(err)
-						}
+						_, err = c.Write(buf[:n])
 					}
 
 					if err != nil {
@@ -125,4 +119,78 @@ func TestProxy(t *testing.T) {
 			}(conn)
 		}
 	}()
+
+	// proxyServer proxies messages from client connections to the
+	// destinationServer. Replies from the destinationServer are proxied
+	// back to the clients.
+	proxyServer, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		for {
+			conn, err := proxyServer.Accept()
+			if err != nil {
+				return
+			}
+
+			go func(from net.Conn) {
+				defer from.Close()
+
+				to, err := net.Dial("tcp", server.Addr().String())
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				defer to.Close()
+
+				err = proxy(from, to)
+				if err != nil && err != io.EOF {
+					t.Error(err)
+				}
+			}(conn)
+		}
+	}()
+
+	conn, err := net.Dial("tcp", proxyServer.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgs := []struct{ Message, Reply string }{
+		{"ping", "pong"},
+		{"pong", "pong"},
+		{"echo", "echo"},
+		{"ping", "pong"},
+	}
+
+	for i, m := range msgs {
+		_, err = conn.Write([]byte(m.Message))
+		if err != nil {
+			t.Fatal(err)
+		}
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+		actual := string(buf[:n])
+		t.Logf("%q -> proxy -> %q", m.Message, actual)
+		if actual != m.Reply {
+			t.Errorf("%d: expected reply: %q; actual: %q",
+				i, m.Reply, actual)
+		}
+	}
+
+	_ = conn.Close()
+	_ = proxyServer.Close()
+	_ = server.Close()
+
+	wg.Wait()
 }
