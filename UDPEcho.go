d7193583f1a8ba4c4b3ea1ec3ef13617f7d2964d
Kaer Tala <khair_talap@outlook.com>
2025-06-21 10:57:21 +0500

    Test for the UDP Echo

--- UDPEcho.go
+++ UDPEcho.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net"
+	"testing"
 )
 
 // echoServerUDP starts a simple UDP echo server.
@@ -60,4 +62,39 @@ func echoServerUDP(ctx context.Context, addr string) (net.Addr, error) {
 
 	// Return the actual address we're bound to (useful when using ":0")
 	return s.LocalAddr(), err
+}
+
+func TestEchoServerUDP(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	serverAddr, err := echoServerUDP(ctx, "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cancel()
+
+	client, err := net.ListenPacket("udp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = client.Close() }()
+
+	msg := []byte("ping")
+	_, err = client.WriteTo(msg, serverAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := make([]byte, 1024)
+	n, addr, err := client.ReadFrom(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if addr.String() != serverAddr.String() {
+		t.Fatalf("received reply from %q instead of %q", addr, serverAddr)
+	}
+
+	if !bytes.Equal(msg, buf[:n]) {
+		t.Errorf("expected reply %q; actual reply %q", msg, buf[:n])
+	}
 }
