0313ae26b67dea5d6c7798825a30151d8a298b3d
Kaer Tala <khair_talap@outlook.com>
2025-06-23 08:33:48 +0500

    Next part of the test

--- UDPEcho.go
+++ UDPEcho.go
@@ -112,4 +112,20 @@ func TestListenPacketUDP(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer func() { _ = client.Close() }()
+
+	interloper, err := net.ListenPacket("udp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	interrupt := []byte("pardon me")
+	n, err := interloper.WriteTo(interrupt, client.LocalAddr())
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = interloper.Close()
+
+	if l := len(interrupt); l != n {
+		t.Fatalf("wrote %d bytes of %d", n, l)
+	}
 }
