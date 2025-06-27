30591b24173b8d2fcc08b14fc6a1124d6fbc344a
Kaer Tala <khair_talap@outlook.com>
2025-06-27 10:01:56 +0500

    TestDialUDP beginning

--- UDPEcho.go
+++ UDPEcho.go
@@ -208,3 +208,18 @@ func TestListenPacketUDP(t *testing.T) {
 			serverAddr, addr)
 	}
 }
+
+func TestDialUDP(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	serverAddr, err := echoServerUDP(ctx, "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cancel()
+
+	client, err := net.Dial("udp", serverAddr.String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = client.Close() }()
+}
