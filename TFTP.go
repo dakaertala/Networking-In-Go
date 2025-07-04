8d4b6f252e1f6c618b38a48c1a723e0cf71d3868
Kaer Tala <khair_talap@outlook.com>
2025-07-04 07:27:51 +0500

    Marshal binary implementation

--- TFTP.go
+++ TFTP.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 const (
 	DatagramSize = 516              // the maximum supported datagram size
 	BlockSize    = DatagramSize - 4 // the DatagramSize minus a 4-byte reader
@@ -28,4 +33,47 @@ const (
 	ErrNoUser
 )
 
+type ReadReq struct {
+	Filename string
+	Mode     string
+}
 
+func (q ReadReq) MarshalBinary() ([]byte, error) {
+	mode := "octet"
+	if q.Mode != "" {
+		mode = q.Mode
+	}
+
+	// operation code + filename + 0 byte + mode + 0 byte
+	cap := 2 + 2 + len(q.Filename) + 1 + len(q.Mode) + 1
+
+	b := new(bytes.Buffer)
+	b.Grow(cap)
+
+	err := binary.Write(b, binary.BigEndian, OpRRQ)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteString(q.Filename)
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.WriteByte(0)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteString(mode)
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.WriteByte(0)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
